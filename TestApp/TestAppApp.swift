35e5526387605632f67ade3ac6d4177e8e5dab32
csehuman <paullee20204@snu.ac.kr>
2022-11-17 14:57:02 +0900

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Paul Lee on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
